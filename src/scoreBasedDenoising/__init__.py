a3d2ed5e27fc4e55a1aec906464c62247fdd8e70
Daniel Utt <utt@ovito.org>
2023-08-15 16:26:28 +0200

    Update to new graphite interface

--- src/scoreBasedDenoising/__init__.py
+++ src/scoreBasedDenoising/__init__.py
@@ -1,8 +1,12 @@
-# Score-based denoising for atomic structure identification
+#### Score-based denoising for atomic structure identification
 # Documentation: https://github.com/nnn911/ScoreBasedDenoising
+#
 # Wrapper around the original implementation at LLNL: https://github.com/LLNL/graphite
+#
 # Reference: https://arxiv.org/abs/2212.02421
 
+import importlib.resources as impRes
+import sys
 import time
 import warnings
 from pathlib import Path
@@ -10,19 +14,23 @@ from pathlib import Path
 import numpy as np
 import torch
 from graphite.nn.models.e3nn_nequip import NequIP
+from graphite.nn.utils.e3nn_initial_embedding import InitialEmbedding
 from graphite.transforms import PeriodicRadiusGraph
-from ovito.data import NearestNeighborFinder, DataTable
+from ovito.data import DataTable, NearestNeighborFinder
 from ovito.io.ase import ovito_to_ase
 from ovito.pipeline import ModifierInterface
 from sklearn.preprocessing import LabelEncoder
 from torch_geometric.data import Data
 from traits.api import Enum, Float, Int, Union
 
-import scoreBasedDenoising
-from scoreBasedDenoising.InitialEmbedding import InitialEmbedding
+# Make InitialEmbedding visible to torch.load() (pre-trained models expect Initial embedding to be part of main)
+setattr(sys.modules["__main__"], "InitialEmbedding", InitialEmbedding)
 
-warnings.filterwarnings("ignore", category=UserWarning,
-                        message="The TorchScript type system doesn't support")
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    message="The TorchScript type system doesn't support",
+)
 
 
 class ScoreBasedDenoising(ModifierInterface):
@@ -113,15 +121,8 @@ class ScoreBasedDenoising(ModifierInterface):
         return np.mean(np.linalg.norm(neighVec, axis=2))
 
     def setupSiO2model(self, data):
-        numSpecies = 2
-        model_path = Path(
-            Path(scoreBasedDenoising.__file__).parent, "pretrained_models"
-        )
-        model = ScoreBasedDenoising.getModel(numSpecies)
-        model.load_state_dict(
-            torch.load(Path(model_path, "SiO2-denoiser.state_dict.pt"))
-        )
-
+        modelDir = impRes.files('graphite.pretrained_models.denoiser')
+        model = torch.load(modelDir.joinpath("SiO2-denoiser.pt"))
         cts = {"Si": 0, "O": 0}
         for uni in np.unique(data.particles["Particle Type"]):
             name = data.particles["Particle Type"].type_by_id(uni).name
@@ -138,34 +139,27 @@ class ScoreBasedDenoising(ModifierInterface):
         return model
 
     def setupFccBccHcpModel(self, data):
-        numSpecies = 1
-        model_path = Path(
-            Path(scoreBasedDenoising.__file__).parent, "pretrained_models"
-        )
-        model = ScoreBasedDenoising.getModel(numSpecies)
-        model.load_state_dict(torch.load(
-            Path(model_path, "Cu-denoiser.state_dict.pt")))
-
+        modelDir = impRes.files('graphite.pretrained_models.denoiser')
+        model = torch.load(modelDir.joinpath("Cu-denoiser.pt"))
         data.particles_.create_property(
             "Particle Type Backup", data=data.particles["Particle Type"]
         )
         data.particles_["Particle Type_"][...] = 1
-
         return model
 
     def teardownFccBccHcpModel(self, data):
-        data.particles_[
-            "Particle Type_"][...] = data.particles["Particle Type Backup"]
+        data.particles_["Particle Type_"][...] = data.particles["Particle Type Backup"]
         del data.particles_["Particle Type Backup"]
 
     @staticmethod
     def writeTable(data, y, ylabel, title):
         table = data.tables.create(
-            identifier=title.replace(" ", "_"), plot_mode=DataTable.PlotMode.Line, title=title)
-        table.x = table.create_property(
-            'Step', data=np.arange(len(y)))
-        table.y = table.create_property(
-            ylabel, data=y)
+            identifier=title.replace(" ", "_"),
+            plot_mode=DataTable.PlotMode.Line,
+            title=title,
+        )
+        table.x = table.create_property("Step", data=np.arange(len(y)))
+        table.y = table.create_property(ylabel, data=y)
 
     def modify(self, data, frame, **kwargs):
         match self.structure:
@@ -203,7 +197,7 @@ class ScoreBasedDenoising(ModifierInterface):
             case _:
                 raise NotImplementedError
 
+        ScoreBasedDenoising.writeTable(data, convergence, "Convergence", "Convergence")
         ScoreBasedDenoising.writeTable(
-            data, convergence, "Convergence", "Convergence")
-        ScoreBasedDenoising.writeTable(data, np.log10(
-            convergence), "Log10(Convergence)", "Log Convergence")
+            data, np.log10(convergence), "Log10(Convergence)", "Log Convergence"
+        )
